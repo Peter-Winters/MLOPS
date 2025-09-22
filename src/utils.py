34cc5a6fdfa28579db7464ea648ae7b299c90693
Peter-Winters <pfwinters@gmail.com>
2025-09-22 14:07:51 +0100

    data transformations

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import os
+import sys
+import pickle
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    '''
+    This function saves a python object to a file
+    '''
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
