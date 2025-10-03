3b9d10cf8f24370e7efba4b4d1d8eeed86928887
Peter-Winters <pfwinters@gmail.com>
2025-10-03 11:26:48 +0100

    html edit

--- application.py
+++ application.py
@@ -9,43 +9,34 @@ from src.pipeline.predict_pipeline import PredictPipeline, CustomData
 
 application = Flask(__name__)
 
-## route for homepage
-# @application.route('/')
-# def index():
-#     return render_template('index.html')
+@application.route('/', methods=['GET'])
+def index():
+    return render_template('home.html') 
 
-@application.route('/', methods=['GET','POST'])
+@application.route('/predict_datapoint', methods=['POST'])
 def predict_datapoint():
-    if request.method == 'POST':
-        try:
-            # Retrieving form data
-            data = CustomData(
-                gender=request.form.get('gender'),
-                race_ethnicity=request.form.get('race_ethnicity'),
-                parental_level_of_education=request.form.get('parental_level_of_education'),
-                lunch=request.form.get('lunch'),  
-                test_preparation_course=request.form.get('test_preparation_course'),
-                reading_score=float(request.form.get('reading_score')),
-                writing_score=float(request.form.get('writing_score'))
-            )
-            
-            # convert to dataframe
-            pred_df = data.get_data_as_dataframe()
-            print(pred_df)
+    try:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('race_ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),  
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
+        )
+        
+        pred_df = data.get_data_as_dataframe()
+        predict_pipeline = PredictPipeline(
+            model_path='artifacts/model.pkl',
+            preprocessor_path='artifacts/preprocessor.pkl')
+        
+        results = predict_pipeline.predict(pred_df)
+        return render_template('home.html', results=results[0])
 
-            # prediction
-            predict_pipeline = PredictPipeline(
-                model_path='artifacts/model.pkl',
-                preprocessor_path='artifacts/preprocessor.pkl')
-            results = predict_pipeline.predict(pred_df)
-
-            return render_template('home.html', results=results[0])
-
-        except Exception as e:
-            print(e)
-            return render_template('home.html', error=str(e))
-    else:
-        return render_template('home.html')
+    except Exception as e:
+        print(e)
+        return render_template('home.html', error=str(e))
     
 
 if __name__ == "__main__":
