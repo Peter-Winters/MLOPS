4e006dec242c4ba91a289ca181b047398621a83e
Peter-Winters <pfwinters@gmail.com>
2025-10-14 15:49:26 +0100

    app edits

--- application.py
+++ application.py
@@ -5,6 +5,10 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
+# paths
+model_path = 'artifacts/model.pkl'
+preprocessor_path = 'artifacts/preprocessor.pkl'
+
 application=Flask(__name__)
 
 ## Route for a home page
@@ -24,13 +28,13 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score')))
+            writing_score=float(request.form.get('reading_score')),
+            reading_score=float(request.form.get('writing_score')))
         
-        pred_df=data.get_data_as_data_frame()
+        pred_df=data.get_data_as_dataframe()
         print(pred_df)
-
-        predict_pipeline=PredictPipeline()
+        
+        predict_pipeline=PredictPipeline(model_path=model_path,preprocessor_path=preprocessor_path)
         results=predict_pipeline.predict(pred_df)
         return render_template('home.html',results=results[0])
     
