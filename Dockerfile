e656cfdf313fef5db29745467256988aec6358bc
Peter-Winters <pfwinters@gmail.com>
2025-10-15 12:46:32 +0100

    port edits

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY artifacts/ ./artifacts/
 COPY . .
-
+EXPOSE 8000
 CMD ["gunicorn", "--bind", "0.0.0.0:${PORT}", "application:application"]
--- application.py
+++ application.py
@@ -42,5 +42,5 @@ def predict_datapoint():
     
 
 if __name__=="__main__":     
-    port = int(os.environ.get("PORT", 80)) 
+    port = int(os.environ.get("PORT", 8000)) 
     application.run(host="0.0.0.0",port=port, debug=True)   
